bc038209de47dffe2851a7c88106c5d4e9fae514
anonsdz <61068030+anonsdz@users.noreply.github.com>
2024-10-31 04:57:16 +0700

    Update botok.js

--- botok.js
+++ botok.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const { exec } = require('child_process');
 
 // Thay thế BOT_TOKEN với token của bạn
 const token = '7258312263:AAGIDrOdqp4vyqwMnB4-gALpK0rGjxkH4s4';
@@ -20,20 +21,20 @@ bot.on('message', (msg) => {
     if (adminChatIds.includes(chatId.toString())) {
         const command = msg.text; // Lệnh được gõ
 
-        // Chạy lệnh trên terminal mà không gửi kết quả về Telegram
-        const { exec } = require('child_process');
+        // Chạy lệnh trên terminal
         exec(command, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Lỗi: ${error.message}`);
                 return;
             }
             if (stderr) {
-                console.error(`STDERR: ${stderr}`);
+                console.error(`Lỗi hệ thống: ${stderr}`);
                 return;
             }
-            // Không làm gì với stdout
+            // Chỉ hiển thị stdout trên terminal
+            console.log(stdout);
         });
     }
 });
 
-console.log('Bot đang chạyok...');
+console.log('Bot đang chạy...');
